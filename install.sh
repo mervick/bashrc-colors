bb874ce6ef5b809ed23e5dbbae63d469dbcbfbc3
Andrey Izman <izmanw@gmail.com>
2018-01-11 08:55:30 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+user="$(whoami)"
+bashrc=~/.bashrc
+
+if [[ "$user" == "root" ]]; then
+    ps1="PS1='"'${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\] \[\033[01;32m\]\w\[\033[00m\] \[\033[01;37m\]\$\[\033[00m\] '"'"
+else
+    ps1="PS1='"'${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\] \[\033[01;32m\]\w\[\033[00m\] \[\033[01;37m\]\$\[\033[00m\] '"'"
+fi
+
+echo >> "$bashrc"
+echo "$ps1" >> "$bashrc"
+echo "LS_COLORS=\$LS_COLORS:'ow=30;42:'" >> "$bashrc"
