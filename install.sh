072a6a4d90aada270a78adfbafbfd8e5d8daeba2
Andrey Izman <izmanw@gmail.com>
2023-03-30 20:01:52 +0300

    Add ability to change host

--- install.sh
+++ install.sh
@@ -13,3 +13,8 @@ fi
 echo >> "$bashrc"
 echo "$ps1" >> "$bashrc"
 echo "LS_COLORS=\$LS_COLORS:'ow=30;42:'" >> "$bashrc"
+
+
+if [[ $1 !== "" ]]; then
+    echo 'PS1="$(echo -n "$PS1" | sed '"'"'s/\\h/'$1'/'"'"')"' >> "$bashrc"
+fi
